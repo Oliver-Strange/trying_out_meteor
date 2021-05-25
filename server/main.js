798930780696164a7209258a86109c10d9aea104
Preston Burton <preston.burton@gmail.com>
2021-05-25 15:35:49 -0700

    imports TasksCollection, inserts tasks array

--- server/main.js
+++ server/main.js
@@ -1,9 +1,19 @@
 import { Meteor } from 'meteor/meteor';
+import { TasksCollection } from './../imports/api/TasksCollection';
 
+const insertTask = taskText => TasksCollection.insert({text: taskText });
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
 
 Meteor.startup(() => {
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task'
+    ].forEach(insertTask)
+  }
 });
